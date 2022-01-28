e42820bbfe3315aeeec32a164882c5aba32b93b1
Martin Lonský <martin.lonsky@aputime.com>
2022-01-28 11:17:29 +0100

    update php 8.1 compatibility

--- src/Collection.php
+++ src/Collection.php
@@ -8,8 +8,9 @@ use DusanKasan\Knapsack\Exceptions\InvalidReturnValue;
 use IteratorAggregate;
 use Traversable;
 
-class Collection implements IteratorAggregate, \Serializable, CollectionInterface
-{
+class Collection implements IteratorAggregate, CollectionInterface {
+
+
     use CollectionTrait;
 
     /**
@@ -17,16 +18,17 @@ class Collection implements IteratorAggregate, \Serializable, CollectionInterfac
      */
     protected $input;
 
+
     /**
      * @var callable
      */
     private $inputFactory;
 
+
     /**
      * @param callable|array|Traversable $input If callable is passed, it must return an array|Traversable.
      */
-    public function __construct($input)
-    {
+    public function __construct($input) {
         if (is_callable($input)) {
             $this->inputFactory = $input;
             $input = $input();
@@ -34,69 +36,21 @@ class Collection implements IteratorAggregate, \Serializable, CollectionInterfac
 
         if (is_array($input)) {
             $this->input = new ArrayIterator($input);
-        } elseif ($input instanceof Traversable) {
+        }
+        else if ($input instanceof Traversable) {
             $this->input = $input;
-        } else {
-            throw $this->inputFactory ? new InvalidReturnValue : new InvalidArgument;
+        }
+        else {
+            throw $this->inputFactory ? new InvalidReturnValue() : new InvalidArgument();
         }
     }
 
-    /**
-     * Static alias of normal constructor.
-     *
-     * @param callable|array|Traversable $input
-     * @return Collection
-     */
-    public static function from($input)
-    {
-        return new self($input);
-    }
-
-    /**
-     * Returns lazy collection of values, where first value is $input and all subsequent values are computed by applying
-     * $function to the last value in the collection. By default this produces an infinite collection. However you can
-     * end the collection by throwing a NoMoreItems exception.
-     *
-     * @param mixed $input
-     * @param callable $function
-     * @return Collection
-     */
-    public static function iterate($input, callable $function)
-    {
-        return iterate($input, $function);
-    }
-
-    /**
-     * Returns a lazy collection of $value repeated $times times. If $times is not provided the collection is infinite.
-     *
-     * @param mixed $value
-     * @param int $times
-     * @return Collection
-     */
-    public static function repeat($value, $times = -1)
-    {
-        return repeat($value, $times);
-    }
-
-    /**
-     * Returns a lazy collection of numbers starting at $start, incremented by $step until $end is reached.
-     *
-     * @param int $start
-     * @param int|null $end
-     * @param int $step
-     * @return Collection
-     */
-    public static function range($start = 0, $end = null, $step = 1)
-    {
-        return \DusanKasan\Knapsack\range($start, $end, $step);
-    }
 
     /**
      * {@inheritdoc}
      * @throws InvalidReturnValue
      */
-    public function getIterator()
-    {
+    public function getIterator(): Traversable {
         if ($this->inputFactory) {
             $input = call_user_func($this->inputFactory);
 
@@ -105,7 +59,7 @@ class Collection implements IteratorAggregate, \Serializable, CollectionInterfac
             }
 
             if (!($input instanceof Traversable)) {
-                throw new InvalidReturnValue;
+                throw new InvalidReturnValue();
             }
 
             $this->input = $input;
@@ -114,28 +68,54 @@ class Collection implements IteratorAggregate, \Serializable, CollectionInterfac
         return $this->input;
     }
 
-    /**
-     * {@inheritdoc}
-     */
-    public function serialize()
-    {
-        return serialize(
-            toArray(
-                map(
-                    $this->input,
-                    function ($value, $key) {
-                        return [$key, $value];
-                    }
-                )
-            )
-        );
-    }
 
     /**
-     * {@inheritdoc}
+     * Static alias of normal constructor.
+     *
+     * @param callable|array|Traversable $input
+     * @return Collection
      */
-    public function unserialize($serialized)
-    {
-        $this->input = dereferenceKeyValue(unserialize($serialized));
+    public static function from($input) {
+        return new self($input);
     }
+
+
+    /**
+     * Returns lazy collection of values, where first value is $input and all subsequent values are computed by applying
+     * $function to the last value in the collection. By default this produces an infinite collection. However you can
+     * end the collection by throwing a NoMoreItems exception.
+     *
+     * @param mixed    $input
+     * @param callable $function
+     * @return Collection
+     */
+    public static function iterate($input, callable $function) {
+        return iterate($input, $function);
+    }
+
+
+    /**
+     * Returns a lazy collection of numbers starting at $start, incremented by $step until $end is reached.
+     *
+     * @param int      $start
+     * @param int|null $end
+     * @param int      $step
+     * @return Collection
+     */
+    public static function range($start = 0, $end = null, $step = 1) {
+        return \DusanKasan\Knapsack\range($start, $end, $step);
+    }
+
+
+    /**
+     * Returns a lazy collection of $value repeated $times times. If $times is not provided the collection is infinite.
+     *
+     * @param mixed $value
+     * @param int   $times
+     * @return Collection
+     */
+    public static function repeat($value, $times = -1) {
+        return repeat($value, $times);
+    }
+
 }
